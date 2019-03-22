a2f160fcc8a73a4804ebfa245ff3aaa9b3db2181
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-03-22 22:34:54 +0300

    added some things to web

--- front/src/router.js
+++ front/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Master from './views/Master.vue'
 
 Vue.use(Router)
 
@@ -12,12 +13,12 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/master',
+      name: 'master',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Master
     }
   ]
 })
---
+++ front/src/views/Master.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="master">
+    <h1>This is a master page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Master'
+}
+</script>
+
+<style scoped>
+
+</style>
+
