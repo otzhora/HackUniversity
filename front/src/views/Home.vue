2e9afb0d4223e653e79a1f9203a54e4efb440bc0
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-03-22 21:55:50 +0300

    added front

---
+++ front/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <MainPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainPage from '@/components/MainPage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    MainPage
+  }
+}
+</script>
