2a590a9ef1c17d571855f2de59e9d9b88f48c466
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-24 01:46:36 +0300

    concat music convert to mp3

--- server/concatenate_music.py
+++ server/concatenate_music.py
@@ -2,6 +2,7 @@ import librosa
 from file_manager import FileManager
 import os
 import numpy as np
+import subprocess
 
 def concat_music(ids, files, outputFile):
     res = []
@@ -9,9 +10,9 @@ def concat_music(ids, files, outputFile):
     curr_dur = 0
 
     for id_ in ids:
-        y, sr = librosa.load(files[id_]['path'], sr=44100)
+        y, sr = librosa.load(files[id_]['path'], sr=22050)
         res = np.concatenate((res, y))
-        curr_dur += librosa.core.get_duration(y, 44100)
+        curr_dur += librosa.core.get_duration(y, 22050)
 
         annotation.append({
             'time': curr_dur, 
@@ -19,8 +20,9 @@ def concat_music(ids, files, outputFile):
         })
     print(res)
     print(len(res))
-    print(librosa.core.get_duration(res, 44100))
-    librosa.output.write_wav(outputFile, res, 44100)
+    print(librosa.core.get_duration(res, 22050))
+    librosa.output.write_wav('buf.wav', res, 22050)
+    subprocess.call(["ffmpeg", "-i", "buf.wav", "-vn", "-ar", "22050", "-ac", "2", "-b:a", "192k", "-f", "mp3", outputFile])
     return annotation
 
 
@@ -32,6 +34,6 @@ if __name__ == '__main__':
 
     ids = list(files.keys())
 
-    ann = concat_music(ids[5:8], files, 'libr_test.wav')
+    ann = concat_music(ids[5:8], files, 'libr_test.mp3')
     print(ann)
     
