64ae67e065feeb1e7aae2ac1e09311ea3c7e8e8d
Vladimir Kocheryzhkin <vladimir.kocheryzhkin@technekes.com>
2019-03-23 16:06:05 +0300

    add play, stop

--- front/src/components/Track.vue
+++ front/src/components/Track.vue
@@ -1,16 +1,11 @@
 <template>
     <div class="track">
         <a class="panel-block is-active" @click="sendToPlayer">
-                
-                
             {{track.title}}
-        
-        
             <img class="right" src="https://img.icons8.com/material/24/000000/play.png">
-                    
-                
-                
-                
+        </a>
+        <a class="panel-block is-active" @click="stopToPlayer">
+            <img class="right" src="https://img.icons8.com/material/24/000000/stop.png">
         </a>
     </div>
 </template>
@@ -21,7 +16,14 @@ export default {
     props: ["track"],
     methods: {
         sendToPlayer: function(){
-            alert(this.track.title + " sent to player")
+            // alert(this.track.title + " sent to player")
+            window.p1 = new window.Tone.Player("http://localhost:5000/img/uploads/test.mp3", function() {
+                window.p1.start();
+            }).toMaster();
+            
+        },
+        stopToPlayer: function() {
+            window.p1.stop()
         }
     }
 }
