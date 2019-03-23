033114b828c63441d8e0eeba30e6ec3404b37a43
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-23 15:07:44 +0300

    file manager

--- server/file_manager.py
+++ server/file_manager.py
@@ -1,12 +1,45 @@
+from uuid import uuid4
+import os
+
 class FileManager: 
     def __init__(self):
-        pass
+        self.files = {}
 
-    def register_new_file(self, file_path, file_title=None):
-        pass
+
+    def register_new_file(self, file_path, file_title=None, file_properties=None):
+        id_ = uuid4()
+        self.files[id_] = {
+            'path': file_path
+        }
+        if file_title:
+            self.files[id_]['title'] = file_title
+        if file_properties:
+            self.files[id_]['properties'] = file_properties
+
 
     def get_registred_files(self):
-        pass
+        return self.files
 
-    def delete_file(self, file_path):
-        pass
+
+    def get_registred_file(self, file_id):
+        return self.files[file_id]
+
+if __name__ == '__main__':
+    fm = FileManager()
+
+    fold_path = os.getcwd() + 'static/music/'
+
+    file1 = 'test1_output.wav'
+    file2 = 'test2.wav'
+
+    fm.register_new_file(fold_path + file1, 'test')
+
+    print(fm.get_registred_files())
+
+    fm.register_new_file(fold_path + file2, 'test2', {'test': 'et'})
+
+    files = fm.get_registred_files()
+    print(files)
+    print(list(files.keys())[1])
+    
+    print(fm.get_registred_file(list(files.keys())[1]))
