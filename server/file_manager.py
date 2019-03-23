997cc70baa129d3fb71e30a36fa2df27da0ad385
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-23 21:52:15 +0300

    saving files info to json

--- server/file_manager.py
+++ server/file_manager.py
@@ -3,6 +3,7 @@ import uuid
 import os
 from os import listdir
 from os.path import isfile, join
+import json
 
 def allowed_file(filename, ALLOWED_EXT):
     return '.' in filename and \
@@ -14,12 +15,21 @@ def get_title(filename):
 class FileManager: 
     def __init__(self, base_path=None, av_ext=None):
         self.files = {}
-        
-        if base_path and av_ext:
-            onlyfiles = [f for f in listdir(base_path) if isfile(join(base_path, f))]
-            for file in onlyfiles:
-                if allowed_file(file, av_ext):
-                    self.register_new_file(base_path + file, get_title(file))
+
+        print(os.getcwd())
+        if os.path.isfile(os.getcwd() + 'files_backup.json'):
+            print('reading data from files_backup.json')
+            with open('files_backup.json', 'r') as f:
+                self.files = json.load(f)
+        else:
+            if base_path and av_ext:
+                onlyfiles = [f for f in listdir(base_path) if isfile(join(base_path, f))]
+                for file in onlyfiles:
+                    if allowed_file(file, av_ext):
+                        self.register_new_file(base_path + file, get_title(file))
+
+            with open('files_backup.json', 'w') as outfile:
+                json.dump(self.files, outfile)
 
 
 
@@ -32,8 +42,13 @@ class FileManager:
         
         if file_title:
             self.files[id_]['title'] = file_title
+        else:
+            self.files[id_]['title'] = get_title(file_path.split('/')[-1])
         if file_properties:
             self.files[id_]['properties'] = file_properties
+
+        with open('server/files_backup.json', 'w') as outfile:
+            json.dump(self.files, outfile)
         return id_
 
     def get_registred_files(self):
