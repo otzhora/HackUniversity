66da466937c260b84162d1cb3336a85c2ed7d987
Vladimir Kocheryzhkin <vladimir.kocheryzhkin@technekes.com>
2019-03-23 17:20:27 +0300

    fix buttons

--- front/src/components/Track.vue
+++ front/src/components/Track.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="track">
-        <a class="panel-block is-active" @click="sendToPlayer">
-            {{track.title}}
-            <img class="right" src="https://img.icons8.com/material/24/000000/play.png">
-        </a>
-        <a class="panel-block is-active" @click="stopToPlayer">
-            <img class="right" src="https://img.icons8.com/material/24/000000/stop.png">
+        <a class="panel-block is-active" >
+            <span style="max-width:300px">{{track.title}}</span>
+            <img class="right" src="https://img.icons8.com/material/24/000000/play.png" @click="play">
+            <img class="right" src="https://img.icons8.com/material/24/000000/stop.png" @click="stop">
+            <img class="right" src="https://img.icons8.com/material/24/000000/pause.png" @click="pause">
         </a>
     </div>
 </template>
@@ -15,13 +14,17 @@ export default {
     name: "Track",
     props: ["track", "url"],
     methods: {
-        sendToPlayer: function(){
+        play: function(){
+            console.log(this.track.url)
             window.p1 = new window.Tone.Player(this.track.url, function() {
                 window.p1.start();
             }).toMaster();
             
         },
-        stopToPlayer: function() {
+        stop: function() {
+            window.p1.stop()
+        },
+        pause: function() {
             window.p1.stop()
         }
     }
