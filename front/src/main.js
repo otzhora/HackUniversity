7832fc61f5b6d51d6d656ff5e299a85c54d3ed35
Vladimir Kocheryzhkin <vladimir.kocheryzhkin@technekes.com>
2019-03-23 14:17:10 +0300

    remove from main.js

--- front/src/main.js
+++ front/src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-https://cdnjs.cloudflare.com/ajax/libs/tone/13.0.1/Tone.min.js
 
 require('./assets/main.scss');
 Vue.config.productionTip = false
