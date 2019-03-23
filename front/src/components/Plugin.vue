a938cd8f32d2bd62365bb561f27340f4909377b2
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-03-23 21:28:13 +0300

    fixed krutilka

--- front/src/components/Plugin.vue
+++ front/src/components/Plugin.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="plugin" id='p1'>
-        <div class="knob">
+        <div class="knobBG">
+            <div class="knob">
+            </div>
         </div>
+        
     </div>
 </template>
 
@@ -10,17 +13,23 @@ import Draggable from 'gsap/Draggable'
 export default {
     name: "Plugin",
     mounted(){
+        let knob = document.querySelector('.knob')
+        console.log(knob)
         Draggable.create(".knob", {type: "rotation", throwProps: true});
     }
 }
 </script>
 
 <style scoped>
-.knob {
+.knobBG, .knob {
   position:absolute;
   background-image: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/knob_Base.png');
   width:355px; 
   height:355px;
-  align-content: right;
+}
+.knob{
+  background-image: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/knob_Spinner.png');
+  z-index: 1;
+
 }
 </style>
