425a72cb6bd02d0decf81589bce347720a589020
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-23 14:52:36 +0300

    file manager prototype

---
+++ server/file_manager.py
@@ -0,0 +1,12 @@
+class FileManager: 
+    def __init__(self):
+        pass
+
+    def register_new_file(self, file_path, file_title=None):
+        pass
+
+    def get_registred_files(self):
+        pass
+
+    def delete_file(self, file_path):
+        pass
