33bccf10f4618accd391ca3e9c1dc134cc3cacf5
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-03-24 00:36:32 +0300

    sr typo fix

--- server/audio.py
+++ server/audio.py
@@ -3,11 +3,15 @@ import librosa
 def get_bmp(file_path, bpm_guess=120):
     # on most tracks works fine w\ guess 120
 
-    y, sr = librosa.load(file_path, sr=44000)
+    y, sr = librosa.load(file_path, sr=44100)
     hop_length = 512
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr, hop_length=hop_length, start_bpm=bpm_guess)
 
     return tempo, beats
+
+
+def concatenate_music(files):
+    pass
 
 if __name__ == '__main__':
     path = '/Users/yra/Desktop/study/hack_university/server/static/music/Queen_-_Dont_Stop_Me_Now.mp3'
