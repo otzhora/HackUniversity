44751161f6833020d061c45f87d56c4496ba8ac7
ivanblinov2k17 <ivanblinov2015@mail.ru>
2019-03-23 14:18:38 +0300

    otrisovka

--- front/src/components/Track.vue
+++ front/src/components/Track.vue
@@ -3,7 +3,7 @@
         <a class="panel-block is-active" @click="sendToPlayer">
                 
                 
-            {{track.title}}
+            {{track.name}}
         
         
             <img class="right" src="https://img.icons8.com/material/24/000000/play.png">
